313246f22d528f8de648896c5ab2b48f0b6e0da2
Joshua Clark <joshua.clark@txstate.edu>
2014-03-17 22:31:13 -0400

    Implement ajax-y pagination

--- app/assets/javascripts/app/angular-server-table.js
+++ app/assets/javascripts/app/angular-server-table.js
@@ -4,18 +4,50 @@ var app = angular.module('angularServerTable', function() {
 
 app.directive('serverTable', function() {
   return {
-    scope: true,
+    // scope: {'doodads': '='},
     templateUrl: 'templates/table.html',
     replace: true,
-    link: function(elem, attr) {
-      console.log("howdy");
+    link: function(scope, elem, attrs) {
+      elem.find('#previous-page').click(function() {
+        scope.previousPage();
+        scope.$apply();
+      });
+
+      elem.find('#next-page').click(function() {
+        scope.nextPage();
+        scope.$apply();
+      });
     }
   };
 });
 
 app.controller('TableController', function($scope, $http) {
+  $scope.page = 1;
   $scope.headers = ['Name', 'Value'];
   $scope.doodads = $http.get('/doodads.json').then(function(response) {
+    $scope.total = response.data.total;
     $scope.doodads = response.data.doodads;
   });
+
+  $scope.previousPage = function() {
+    $scope.page -= 1;
+    $scope.doodads = $http.get('/doodads.json', {params: {page: $scope.page}}).then(function(response) {
+      $scope.doodads = response.data.doodads;
+    });
+  };
+
+  $scope.nextPage = function() {
+    $scope.page += 1;
+    $scope.doodads = $http.get('/doodads.json', {params: {page: $scope.page}}).then(function(response) {
+      $scope.doodads = response.data.doodads;
+    });
+  };
+
+  $scope.notFirstPage = function() {
+    return $scope.page > 1;
+  };
+
+  $scope.hasMore = function() {
+    return $scope.page * 10 < $scope.total;
+  };
 });
--- app/controllers/doodads_controller.rb
+++ app/controllers/doodads_controller.rb
@@ -1,10 +1,20 @@
 class DoodadsController < ApplicationController
   def index
-    @doodads = Doodad.all
+    @doodads = Doodad.all.limit(10).offset(offset)
 
     respond_to do |format|
       format.html
-      format.json { render json: { doodads: @doodads } }
+      format.json { render json: { 
+        doodads: @doodads,
+        total: Doodad.count
+      } }
     end
+  end
+
+  private
+
+  def offset
+    page = params[:page]
+    10 * (page ? Integer(page) - 1 : 0)
   end
 end
